50b88ebacd8190ba9bb6f64cca927498812090b9
kenurfseet@gmail.com <kenurfseet@gmail.com>
2023-11-08 17:04:51 +0800

    completed ass3

--- Assignment3files/Integrity-A0258173Y.py
+++ Assignment3files/Integrity-A0258173Y.py
@@ -11,17 +11,18 @@ key_file_name   = sys.argv[1]
 file_name       = sys.argv[2]
 
 # get the authentication key from the file
-# TODO
+key_file = open(key_file_name, "rb")
+key = key_file.read()
 
 # read the input file
+input = open(file_name, "rb")
 
 # First 32 bytes is the message authentication code
-# TODO
-mac_from_file = "get me from the file"
+input = input.read()
+mac_from_file = input[:32]
 
 # Use the remaining file content to generate the message authentication code
-# TODO
-mac_generated = "generated from the file"
+mac_generated = SHA256.new(input[32:] + key).digest()
 
 if mac_from_file == mac_generated:
     print ('yes')
